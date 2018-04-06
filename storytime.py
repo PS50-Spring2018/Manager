70a152ede8eb166f2d660e43244ea0bb5f3d434b
mklureza <mklureza@gmail.com>
2018-04-06 16:12:12 -0400

    dragons are cool

--- storytime.py
+++ storytime.py
@@ -2,5 +2,5 @@
 Use this space to collaboratively write a short story with your team.
 After every few sentences, push your changes to the remote repository and resolve conflicts.
 
-YOUR STORY HERE
+Once upon a time, there was a dragon.
 '''
