81082ea4aa7d70821625ae71fd983b648577dd2d
Nicole Kim <nkim1322@gmail.com>
2018-04-06 16:18:30 -0400

    more content

--- storytime.py
+++ storytime.py
@@ -3,4 +3,5 @@ Use this space to collaboratively write a short story with your team.
 After every few sentences, push your changes to the remote repository and resolve conflicts.
 
 Once upon a time, there was a dragon.  Her name was Hannah.  The dragon was very small, but very fierce.  
-She wanted to become a scuba diver, but unfortunately her fire did not mesh well with the ocean.
+She wanted to become a scuba diver, but unfortunately her fire did not mesh well with the ocean #yikes. 
+She instead decided to learn to code. She heard of this class called PS51 at Yale University. 
