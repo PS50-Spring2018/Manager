f2ec6ae073eb81050c494daed75cb58b95169b2d
mklureza <mklureza@gmail.com>
2018-04-06 16:27:25 -0400

    just details

--- storytime.py
+++ storytime.py
@@ -5,8 +5,10 @@ After every few sentences, push your changes to the remote repository and resolv
 Once upon a time, there was a dragon.  Her name was Hannah.  The dragon was very small, but very fierce.  
 She wanted to become a scuba diver, but unfortunately her fire did not mesh well with the ocean #yikes. 
 Hannah decided that she would have to build a custom scuba suit, lined with python skin, but that didn't turn out that well.
-She instead decided to learn to code. She heard of this class called PS51 at Yale University. 
+Unfortunately, pythons are not particularly tractable when one is a small dragon.  Hannah flamed with frustration and then started doing research.
 
-^^ The above is what I currently see
+She instead decided to learn to code. She heard of this class called PS51 at Yale University.
+Maybe that was where she could learn to bend python to her will?
+
 
 '''
