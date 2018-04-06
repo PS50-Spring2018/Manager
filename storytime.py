80f69362735fa054ababd7c81ab9c5844b471235
mklureza <mklureza@gmail.com>
2018-04-06 16:24:18 -0400

    oops

--- storytime.py
+++ storytime.py
@@ -7,12 +7,4 @@ She wanted to become a scuba diver, but unfortunately her fire did not mesh well
 Hannah decided that she would have to build a custom scuba suit, lined with python skin, but that didn't turn out that well.
 She instead decided to learn to code. She heard of this class called PS51 at Yale University. 
 
-
-
-
-Hannah decided that she would have to build a custom scuba suit, lined with python skin.  
-Unfortunately, pythons are not particularly tractable when one is a small dragon.  Hannah flamed with frustration and then started doing research.
-She decided to take PS 50, as that seemed to be where she could learn to bend python to her will.
-
-
 '''
