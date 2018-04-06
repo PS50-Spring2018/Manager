6c72ac9d65ce8d99cb361b8496a68861fd15e05d
mklureza <mklureza@gmail.com>
2018-04-06 16:22:22 -0400

    it's a stretch and I'm not even sorry

--- storytime.py
+++ storytime.py
@@ -5,5 +5,9 @@ After every few sentences, push your changes to the remote repository and resolv
 Once upon a time, there was a dragon.  Her name was Hannah.  The dragon was very small, but very fierce.  
 She wanted to become a scuba diver, but unfortunately her fire did not mesh well with the ocean.
 
-Hannah decided that she would have to build a custom scuba suit, lined with python skin.
+Hannah decided that she would have to build a custom scuba suit, lined with python skin.  
+Unfortunately, pythons are not particularly tractable when one is a small dragon.  Hannah flamed with frustration and then started doing research.
+She decided to take PS 50, as that seemed to be where she could learn to bend python to her will.
+
+
 '''
