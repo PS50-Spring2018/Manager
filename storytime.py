f7088db45b8fb4439c7b00f2c9668bc524c66395
mklureza <mklureza@gmail.com>
2018-04-06 16:16:14 -0400

    plot, maybe?

--- storytime.py
+++ storytime.py
@@ -2,5 +2,6 @@
 Use this space to collaboratively write a short story with your team.
 After every few sentences, push your changes to the remote repository and resolve conflicts.
 
-Once upon a time, there was a dragon.
+Once upon a time, there was a dragon.  The dragon was very small, but very fierce.  
+She wanted to become a scuba diver, but unfortunately her fire did not mesh well with the ocean.
 '''
