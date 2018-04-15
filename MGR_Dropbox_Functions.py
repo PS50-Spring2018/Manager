56789ba9027b663bec71705f6c7c1dccaab6565f
Nicole Kim <nkim1322@gmail.com>
2018-04-15 15:41:47 -0400

    new file for Dropbox functions

---
+++ MGR_Dropbox_Functions.py
@@ -0,0 +1,7 @@
+# Script for Dropbox functions: 
+	# (1) Check dropbox for new image files 
+	# (2) Download new image files and redownload CSV of relevant image information
+
+# Function: Check dropbox for new image files
+
+# Function: Download new image files and redownload CSV of relevant image information
