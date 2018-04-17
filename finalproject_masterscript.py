51f5cc613cc19b9700826be00d604a4071e76a58
mklureza <mklureza@gmail.com>
2018-04-17 18:30:23 -0400

    minor group 3 details

--- finalproject_masterscript.py
+++ finalproject_masterscript.py
@@ -81,7 +81,7 @@ experimentnamesomehowwhatdowewanttocallthis = Experiment(imagelist)
 
 #call group 3 analysis functions on experiment object - what inputs are they using specifically/what's their syntax?
 	#first import their function
-from group3filename import whateveritis, whatever2, whatever3
+from PS50DataAnalysis import *
 	#a - graph of R, G, B vs. iteration (?)
 	#b - actual image
 	#c - avg RGB path, mapped onto color wheel
