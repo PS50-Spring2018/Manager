b906100e57fccf30cfda2b70058faa65b8e7cf2e
Nicole Kim <nkim1322@gmail.com>
2018-04-18 20:06:37 -0400

    dropbox function updates, still working onit though

--- MGR_Dropbox_Functions.py
+++ MGR_Dropbox_Functions.py
@@ -3,27 +3,32 @@
 	# (2) Download new image files and redownload CSV of relevant image information
 
 
-# Create Python Dropbox API, source: https://www.blog.pythonlibrary.org/2013/07/17/connecting-to-dropbox-with-python/
-# Use module os (ask Hannah), within Python script access Terminal, use cd to change to (or input: what is the path to the dropbox)
-# pwd 
+
+# Use module os, use Terminal, use cd to change to (or input: what is the path to the dropbox)
+
+# Change working directory to Dropbox
 
 import os
 
 path = '/Users/nicolekim/Dropbox'
 
+dirval = os.getcwd()
+print("Current working directory %s" % dirval)
+
 os.chdir(path)
+# Check current working directory.
+dirval = os.getcwd()
+
+print("Directory changed successfully %s" % dirsval)
 
 
 
-
-
-# Function: Check dropbox for new image files
+# Function: Check dropbox for new image files and if yes, download new images files and redownload CSV
 # Webcam interface filename convention: eg 20180410175530 = year, month, date, hour (24 hour), minute, seconds
+last_im = ' '
 
+if 
 
-
-
-# Function: Download new image files and redownload CSV of relevant image information
 
 # Save to csv file - each row represents a different image and columns contain info on that image
 # Update csv file in real time
