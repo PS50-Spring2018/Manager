b78f55765c97184f67d00788c44b4c5c4fdb2720
mklureza <mklureza@gmail.com>
2018-04-20 09:23:58 -0400

    working to incorporate new functions into masterscript

--- finalproject_masterscript.py
+++ finalproject_masterscript.py
@@ -25,7 +25,9 @@ print(total_images)
 
 #import Dropbox functions
 from MGR_Dropbox_functions import filecheck
-from constructorfunctions import getImageraw, getImagecontourdata
+from constructorfunctions import *
+from meanvariancerawcontourimagesfunctions import *
+
 #import camera controls
 from CameraOps import stream, snap
 
@@ -36,8 +38,8 @@ for i in range(0,total_images):
 	snap()		#output of this function is a file saved to dropbox, yes?
 
 	#go ahead and download this from dropbox
-	getImageraw()			#where does this save to? need to know in order to implement Image class
-	getImagecontourdata()	#same question as above
+	image_array = getrawImage()			
+	contourimage_array = getImagecontourdata()
 
 	#wait the appropriate amount of time before taking the next image
 	time.sleep(60/rate)
@@ -46,33 +48,26 @@ for i in range(0,total_images):
 
 #Import image class - file currently called "constructor", may want to rename for clarity
 from constructor import Image
-#next import functions
-from constructorfunctions import *
 
 #create an Image object for each image file downloaded from Dropbox
 imagelist = []
 for file in whateverfolderitis: 		#FIX this
-	#open file
-	f = open(filename,'r')
-
-	#read whatever line it is that has the values we want: FIX THIS
-	line = f.readline() 
+	getMeanVariance()
 
 	#read necessary components of file to get inputs for image class: FIX THIS
 	red = 
 	green = 
 	blue = 
-	meanred = getmeanRed()
-	meangreen = getmeanGreen()
-	meanblue = getmeanBlue()
-	variancered = getvarianceRed()
-	variancegreen = getvarianceGreen()
-	varianceblue = getvarianceBlue()
+	meanred = 
+	meangreen = 
+	meanblue = 
+	variancered = 
+	variancegreen = 
+	varianceblue = 
 
 	#create instance of Image
 	uniqueimagenamesomehow = Image(red, green, blue, meanred, meangreen, meanblue, variancered, variancegreen, varianceblue)
-	#close file
-	f.close()
+
 	# append image object to list of images
 	imagelist.append(uniqueimagenamesomehow)
 
