9a5ec0313538cd41fbd26a9d69de88235bf9f5a2
Nicole Kim <nkim1322@gmail.com>
2018-04-24 14:49:48 -0400

    Organized repo into folders, all relevant code in Master2

--- Test_image_run.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-testimagearray = np.load('sampleimage.npy')
-
-print(testimagearray)
-
-# To run in Terminal, make sure you are in the Manager repo folder and type "python 'Test_image_run.py' " 
-# and the output is the np array
